c3ff8f9ff5b0a0d1d5d611db2574dc05982b7d7d
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-11-17 12:13:09 +0100

    created connection to db

---
+++ database/knexfile.js
@@ -0,0 +1,49 @@
+process.env.NODE_CONFIG_DIR = require('path').join(__dirname, '../config');
+require('dotenv').config({ path: '../.env' });
+const config = require('config');
+
+module.exports = {
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: config.get('database.host'),
+            port: config.get('database.port'),
+            user: config.get('database.user'),
+            password: config.get('database.password'),
+            database: config.get('database.name'),
+        },
+        pool: { min: 0, max: 7 },
+    },
+
+    staging: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+
+    production: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password',
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./database/db');
 
 const app = require('express')();
 const config = require('config');
