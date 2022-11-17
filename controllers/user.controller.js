b89d7fa092e039b81ede9e83cf0133bda606d177
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-11-17 17:42:34 +0100

    add user validator

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,19 +2,10 @@ const debug = require('debug')('app:userCtrl');
 const User = require('../models/user.models');
 
 class UserController {
-    mapToDataModel(data) {
-        return {
-            firstName: data.firstName,
-            lastName: data.lastName,
-            email: data.email,
-            password: data.password,
-        };
-    }
-
     async createUser(userDTO) {
         try {
             const newUser = await User.query().insert(
-                this.mapToDataModel(userDTO)    
+                this.#mapToDataModel(userDTO)
             );
             return {
                 success: true,
@@ -26,6 +17,16 @@ class UserController {
             return exception;
         }
     }
+
+    #mapToDataModel(data) {
+        return {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            password: data.password,
+        };
+    }
+    
 }
 
 module.exports = new UserController();
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
+const userValidators = require('../validators/user.validator');
 
 router.post('/', async (req, res) => {
-    const newUser = await userController.createUser(req.body);
-
+    const { error, value: userDTO } = userValidators.validateCreate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    const newUser = await userController.createUser(userDTO);
     return res.status(201).send(newUser);
 });
 
