dc32b9b82fca736c07504d31ade6841a40501cd1
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-11-18 14:10:50 +0100

    add transaction model

---
+++ models/transaction.model.js
@@ -0,0 +1,62 @@
+const { Model } = require('objection');
+
+class Transaction extends Model {
+    static get tableName() {
+        return 'transactions';
+    }
+
+    static get accountId() {
+        return 'accountId';
+    }
+
+    static get txnType() {
+        return 'txnType';
+    }
+
+    static get purpose() {
+        return 'purpose';
+    }
+
+    static get amount() {
+        return 'amount';
+    }
+
+    static get reference() {
+        return 'reference';
+    }
+
+    static get balanceBefore() {
+        return 'balanceBefore';
+    }
+
+    static get balanceAfter() {
+        return 'balanceAfter';
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: [
+                'accountId',
+                'txnType',
+                'purpose',
+                'amount',
+                'reference',
+                'balanceBefore',
+                'balanceAfter',
+            ],
+            properties: {
+                id: { type: 'integer' },
+                accountId: { type: 'integer' },
+                txnType: { type: 'string' },
+                purpose: { type: 'string' },
+                amount: { type: 'integer' },
+                reference: { type: 'string' },
+                balanceBefore: { type: 'integer' },
+                balanceAfter: { type: 'integer' },
+            },
+        };
+    }
+}
+
+module.exports = Transaction;
