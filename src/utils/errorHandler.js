63d8a4975d304749b78e656ee14c5ed7eec7c128
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2023-02-09 13:34:01 +0100

    minor fix

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,6 +3,7 @@ const logger = require('../loaders/logger');
 
 class ErrorHandler {
     handleError(error) {
+        console.log(error);
         logger.error(error.message, error.stack);
         // send email to admin if critical
         // send to sentry for monitoring
